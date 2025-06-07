071569f12c1fd0fa6b84b2a7b40da0a1737d0c1b
mero-firstRepository <grgganesh8@gmail.com>
2025-06-07 17:36:12 +0545

    Tic-Tac-Toe winner detaction

---
+++ script.js
@@ -0,0 +1,33 @@
+let boxes = document.querySelectorAll(".box");
+let winningPattern =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],];
+let turnO = true;
+const checkWinner =()=>{
+    for(let pattern of winningPattern){
+        let pOvalue= boxes[pattern[0]].innerText;
+        let p1value= boxes[pattern[1]].innerText;
+        let p2value= boxes[pattern[2]].innerText;
+        if(pOvalue != "" && p1value != "" && p2value != ""){
+            if(pOvalue === p1value && p1value === p2value){
+                console.log("winner");
+            }
+        }
+    }
+}
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("Box was clicked");
+        if(turnO){
+            box.innerText ="O";
+            turnO = false;
+        }
+        else{
+            box.innerText="X";
+            turnO = true;
+        }
+        box.disabled=true;
+        
+        checkWinner(); 
+
+    });
+   
+});
